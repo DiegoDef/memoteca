f37aac25f0603e32c40b16f102c4c08541e1b86d
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2024-09-11 07:21:11 -0300

    add create, list and delete pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -6,23 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: 'modelo 1',
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: '',
   };
-  
+
   criarPensamento() {
-    alert("Criando pensamento")
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['listarPensamento']);
+    });
   }
 
   cancelarPensamento() {
-    alert("Cancelando pensamento")
+    alert('Cancelando pensamento');
   }
 
-  constructor() {}
+  constructor(private service: PensamentoService, private router: Router) {}
 
   ngOnInit(): void {}
 }
